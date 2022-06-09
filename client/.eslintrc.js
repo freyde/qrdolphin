6c2fae98a5463a1f12e843af601c4e1744be19eb
kanamui <chrlzmndz.exe@gmail.com>
2022-06-10 01:05:40 +0800

    implemented web app and native base

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 0,
+    'no-trailing-spaces': 0,
+    'eol-last': 0,
+    'comma-dangle': 0
+  }
 };
---
+++ client/index.native.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import name from './src/app.json';
+
+const { appName } = name;
+AppRegistry.registerComponent('qrmo', () => App);
